5cdde12cfec36704431bf95609b2469e70c841ad
William Bradshaw <will.bradshaw50@gmail.com>
2022-01-17 17:58:23 +1100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Manrope', 'Helvetica', 'sans-serif'],
+      serif: ['Lora', 'serif']
+    },
+    container: {
+      center: true,
+      padding: "1rem"
+    },
+    extend: {
+      colors: {
+        "portfolio-green": "#496248",
+        "portfolio-grey": "#C4C4C4"
+      },
+    },
+  },
+  plugins: [],
+}
